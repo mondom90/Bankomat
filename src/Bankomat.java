f83357a46188fc178e34899f35b33ec48212a603
MandS <monika.domaradzkaa@gmail.com>
2022-11-23 16:20:41 +0000

    Withdrawal

--- src/Bankomat.java
+++ src/Bankomat.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class Bankomat {
+    public static void main(String[] args) {
+        int accountbalance = 1000;
+
+            System.out.println("How much would you like to withdraw");
+            Scanner scanner = new Scanner(System.in);
+            int withdraw = scanner.nextInt();
+            accountbalance = accountbalance - withdraw;
+
+            if (withdraw > 1000) {
+                System.out.println("You do not have enough money in your account, please amend the amount");
+            } else {
+                System.out.println("Amount withdrawn $" + withdraw + ", Balance after the transaction $" + accountbalance );
+            }
+    }
 }
+
+
+
+
